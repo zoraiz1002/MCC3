a601a03c6afc3ede486b137723487fb4835cda3b
pakistanisinmarburg-jpg <pakistanisinmarburg@gmail.com>
2026-05-25 15:25:19 +0200

    crudtable another try

--- src/components/admin/CrudTable.tsx
+++ src/components/admin/CrudTable.tsx
@@ -1,3 +1,4 @@
+import { useState, type ReactNode } from "react";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
